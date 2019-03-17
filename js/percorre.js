2fcc2ee25553b2df28dcf53e0d469de1caa6d691
Ronaldo <41022625+cabralPorto@users.noreply.github.com>
2019-03-17 12:24:42 -0300

    Create percorre.js

---
+++ js/percorre.js
@@ -0,0 +1,9 @@
+//arquivo precorre.js
+function precorreArray(trsPacientes, comportamento) {
+
+    for(var posicaoAtual = 0; posicaoAtual <= trsPacientes.length - 1; posicaoAtual++){
+
+        var pacienteTrAtual = trsPacientes[posicaoAtual];
+        comportamento(pacienteTrAtual);
+    }
+}
